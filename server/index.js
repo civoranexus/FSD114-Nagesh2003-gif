7efe3b6f0a039ca38caf955e30b4f02f56caf516
Nagesh2003-gif <ngshshahane@gmail.com>
2026-01-05 16:41:55 +0530

    Basci Server Setup Done

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./database/db.js";
+
+
+dotenv.config();
+
+
+const app = express();
+
+
+const port = process.env.PORT;
+
+
+app.get("/", (req, res) => {
+  res.send("Server is Running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+  connectDB();
+});
