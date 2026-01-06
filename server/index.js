0998b54002a2a326dfcb7a83b649351ea755988f
Nagesh2003-gif <ngshshahane@gmail.com>
2026-01-06 14:16:50 +0530

    Register API Created and tested using postman

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./database/db.js";
+import userRoutes from "./routes/user.js";
+import cors from "cors";
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 
 dotenv.config();
@@ -11,6 +14,12 @@ const app = express();
 
 const port = process.env.PORT;
 
+// using middlewares
+app.use(express.json());
+app.use(cors());
+
+// using routes
+app.use("/api", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server is Running");
---
+++ server/routes/user.js
@@ -0,0 +1,10 @@
+import express from "express";
+import {
+  register
+} from "../controllers/user.js";
+
+const router = express.Router();
+
+router.post("/user/register", register);
+
+export default router;
